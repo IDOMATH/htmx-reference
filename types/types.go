4117bd8fba0396672ef4cb7b85fa49f4e7eb89e2
IDOMATH <btdothemath@gmail.com>
2024-07-05 23:54:20 -0500

    Adding email validator

--- types/types.go
+++ types/types.go
@@ -9,3 +9,12 @@ type Contact struct {
 	Name  string
 	Email string
 }
+
+func (d *TemplateData) HasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
