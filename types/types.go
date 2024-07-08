3a16286f647fcc1f0293cfe993775e4df0601c00
IDOMATH <btdothemath@gmail.com>
2024-07-07 21:16:07 -0500

    Adding form data function

--- types/types.go
+++ types/types.go
@@ -23,3 +23,10 @@ type FormData struct {
 	Values map[string]string
 	Errors map[string]string
 }
+
+func NewFormData() *FormData {
+	return &FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
